847410782bbf55e362b178f952701e42d556ad28
carojames672 <carolinajames2007@gmail.com>
2025-05-24 16:57:22 -0300

    Add files via upload

---
+++ practica5.cpp
@@ -0,0 +1,31 @@
+// practica5.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Encabezado de La Tabla 
+	cout << "Numero\tCuadrado\tCubo" << endl;
+	// bucle que recorre los numeros del 1 al 10
+	for (int i = 0; i <= 10; i++) {
+		int cuadrado = i * i;
+		int cubo = i * i * i;
+		//Mostrar los resultados en forma de tabla
+		cout << i << "\t" << cuadrado << "\t" << cubo << endl;
+	}
+		return 0;
+		
+		}
+
+
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
